da8f93b3efdc06539e4a76d7808961a1373330c8
othernima08 <desichristine2@gmail.com>
2021-11-27 22:14:18 +0700

    All tables already inserted on system database

---
+++ database/migrations/2021_11_27_150908_create_checkout_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckoutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkout', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamp('tanggalcheckout');
+            $table->unsignedBigInteger('shoppingCartId');
+            $table->string('alamat');
+            $table->string('nomortelepon');
+            $table->string('metodebayar');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkout');
+    }
+}
