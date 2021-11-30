712f44a586931cf83693ebfc61867bc7dae87424
othernima08 <desichristine2@gmail.com>
2021-11-30 09:21:58 +0700

    Add produkcontroller

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\Produk;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ProdukController extends Controller
 {
     public function index(){
-        $produk = DB::table('produk')->get();
-        return view('product',['product' => $produk]);
+        $produk = Produk::all();
+        return view('product',['product' => $produks]);
+    }
+
+    public function addProduk()
+    {
+        $produk = new Produk();
+
+        //model->columnName = request('field_name');
+        $produk->id = \request('id');
+        $produk->tipe = \request('tipe');
+        $produk->manufaktur = \request('manufaktur');
+        $produk->harga = \request('harga');
+        $produk->stok = \request('stok');
+        $produk->terjual = \request('terjual');
+        $produk->garansi = \request('garansi');
+
+        $produk->save();//Insert into table produk(id,tipe, manufaktur, harga, stok, terjual, garansi, created_at,updated_at) value(?,?,?,?,?);
+        return redirect()->route('produkform')->with('success','Produk added successfully');
     }
 }
