23e20b9f50a485a465d0b6a9f5ae551dc6cc7a11
Pijar Karisma <pijarkarisma@gmail.com>
2021-12-17 15:03:20 +0700

    Add email verification

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
-})->name('home');
+})->middleware(['verified'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('landing');
-})->middleware(['auth'])->name('landing');
+})->middleware(['auth','verified'])->name('landing');
 
 Route::get('/admin',function(){
     if (Gate::denies('isAdmin')){
